eb4ecba1ad9dea1d6fed9e97de135558c174cf93
Chris Langhout <clanghout@bol.com>
2021-10-22 12:10:10 +0200

    remove lodash dependencies (fixes build too)

--- src/app/shared/util.ts
+++ src/app/shared/util.ts
@@ -2,4 +2,26 @@ export class Util {
   public static replaceDots(s: string) {
     return s.replace(new RegExp('\\.', 'g'), '-')
   }
+
+  public static isNullOrUndefined(o: any): boolean {
+    return o == null
+  }
+
+  public static isEmpty(obj: object | Array<any>): boolean {
+    // @ts-ignore
+    return [Object, Array].includes((obj || {}).constructor) && !Object.entries((obj || {})).length;
+  }
+
+  public static isObject(value: any): boolean {
+    const type = typeof value
+    return type === 'function' || type === 'object' && !Array.isArray(value) && !!value
+  }
+
+  public static copyString(value: string): string {
+    return value.slice()
+  }
+
+  public static deepCopy(value: object): object {
+    return JSON.parse(JSON.stringify(value))
+  }
 }
